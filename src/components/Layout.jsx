2013cdbc2acf5e3f9c4c34c6e5c91cd43d246e45
yidahc <yidah.a.curiel@gmail.com>
2020-02-22 15:41:14 -0600

    finishing stiling fixes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function (props) {
     return (
         <>
             {/* React Fragment  */}
             <header>
-                <h1 className="heading">Rick <span>And</span> Morty</h1>
+                <Link style={{textDecoration: "none"}} to="/">
+                    <h1 className="heading">Rick <span>And</span> Morty</h1>
+                </Link>
+
             </header>
             <main >
                 {props.children}
